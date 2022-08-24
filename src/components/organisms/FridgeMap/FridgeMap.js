25211c64ab9a3ca13a5d8c969d8e3e92515b4fe2
Weston Norwood <weston.norwood@gmail.com>
2022-08-23 23:06:45 -0700

    feat: Change map marker based on fridge property

--- src/components/organisms/FridgeMap/FridgeMap.js
+++ src/components/organisms/FridgeMap/FridgeMap.js
@@ -7,9 +7,7 @@ import { useState, useEffect } from 'react';
 
 const FridgeMap = (props) => {
   const { fridgeData } = props;
-  const defaultLocation = [40.70580857568261, -73.99646699561376]; // default location Brooklyn Bridge
-  const apollo = [40.80993207736775, -73.95011834412595]; // default location Brooklyn Bridge
-  const centralPark = [40.77211735635662, -73.97360894784433]; // default location Brooklyn Bridge
+  const defaultLocation = [40.70580857568261, -73.99646699561376]; // Brooklyn Bridge
 
   const fullColor = '#5eb66e';
   const serviceColor = '#ff8484';
@@ -22,15 +20,26 @@ const FridgeMap = (props) => {
 
   const fridgeEmptyIcon = Leaflet.divIcon({
     className: 'dummy',
-    html: ReactDOMServer.renderToString(
-      <MapPinIcon fill={emptyColor} sx={{}} />
-    ),
+    html: ReactDOMServer.renderToString(<MapPinIcon fill={emptyColor} />),
   });
 
   const fridgeServiceIcon = Leaflet.divIcon({
     className: 'dummy',
     html: ReactDOMServer.renderToString(<MapPinIcon fill={serviceColor} />),
   });
+  // Testing with borough, because fridge
+  // status isn't part of sample data set
+
+  const fridgeStatusSwitch = (borough) => {
+    switch (borough) {
+      case 'Brooklyn':
+        return fridgeServiceIcon;
+      case 'Queens':
+        return fridgeEmptyIcon;
+      default:
+        return fridgeFullIcon;
+    }
+  };
 
   const fridgeArray =
     Object.keys(fridgeData).length > 1 &&
@@ -44,7 +53,7 @@ const FridgeMap = (props) => {
       const location = [lat, lng];
       console.log(location);
       return (
-        <Marker position={location} icon={fridgeFullIcon} key={i}>
+        <Marker position={location} icon={fridgeStatusSwitch(borough)} key={i}>
           Test
         </Marker>
       );
@@ -65,9 +74,6 @@ const FridgeMap = (props) => {
           subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
         />
         {fridgeMarkers}
-        <Marker position={defaultLocation} icon={fridgeEmptyIcon}></Marker>
-        <Marker position={apollo} icon={fridgeFullIcon}></Marker>
-        <Marker position={centralPark} icon={fridgeServiceIcon}></Marker>
       </MapContainer>
     </Container>
   );
